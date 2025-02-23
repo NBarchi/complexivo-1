e7e5aa897a1a657e3827f677684252a3deafcc0e
nbarchi <nicobarrera.g10@gmail.com>
2025-02-22 20:09:56 -0500

    chore: user model and controller

--- complexivo-app/src/main/java/com/desarrollo/complexivo_app/controllers/UserController.java
+++ complexivo-app/src/main/java/com/desarrollo/complexivo_app/controllers/UserController.java
@@ -1,15 +1,27 @@
 package com.desarrollo.complexivo_app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.desarrollo.complexivo_app.models.User;
+
 
 
 @Controller
 public class UserController {
 
-    @GetMapping("/user")
-    public String getUser(){
-        return "user"; //template
+    @GetMapping("/register")
+    public String getFormRegister(){
+        return "register"; //template
     }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute User user){
+        System.out.println(user.getName());
+        return "register";
+    }
+    
+    
 
 
     
